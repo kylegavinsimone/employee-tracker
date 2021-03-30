0ea3a77b8f326c202cab69a663050c4580eec2de
kylegavinsimone <kylesimone@outlook.com>
2021-03-30 10:01:23 -0700

    instructor helped connect mysql correct

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "",
-  database: " employeeTracker_DB",
+  database: "employeeTracker",
 });
 
 connection.connect();
