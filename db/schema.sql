868451d5e9451a430e9e8282886130a890d98e30
kylegavinsimone <kylesimone@outlook.com>
2021-03-30 09:39:26 -0700

    formatting tables better so I can see line by line what is needed

--- db/schema.sql
+++ db/schema.sql
@@ -13,16 +13,25 @@ CREATE TABLE departments (
 
 CREATE TABLE roles (
 -- id - INT PRIMARY KEY
+ID int NOT NULL,
 -- title - VARCHAR(30) to hold role title
+
 -- salary - DECIMAL to hold role salary
--- department_id - INT to hold reference to department role belongs to    
+
+-- department_id - INT to hold reference to department role belongs to  
+
 )
 
 CREATE TABLE employees (
 -- id - INT PRIMARY KEY
+
 -- first_name - VARCHAR(30) to hold employee first name
+
 -- last_name - VARCHAR(30) to hold employee last name
+
 -- role_id - INT to hold reference to role employee has
+
 -- manager_id - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+
     
 )
