c1675af9e17d6e0da6a18871b4a9c2dceed6bb23
kylegavinsimone <kylesimone@outlook.com>
2021-03-30 16:13:58 -0700

    seeds set up

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,20 @@ INSERT INTO department (name) VALUES
 ('Foreman'),
 ('Worker'),
 ;
+
+INSERT INTO role (title, department_id) VALUES 
+('Boss', 1),
+('Supervisor', 2),
+('Recruit', 3),
+;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+('Parker', 'Peter', 1, null),
+('Grey', 'Jean', 1, 1),
+
+('Summers', 'Scott', 2, null),
+('Brock', 'Eddie', 2, 2),
+
+('Rodgers', 'Steve', 3, null),
+('Stark', 'Tony', 3, 3),
+
