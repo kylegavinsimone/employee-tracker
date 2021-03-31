0f0a719c422d4bd3059b88e96aec0471a9b199eb
kylegavinsimone <kylesimone@outlook.com>
2021-03-30 17:39:10 -0700

    syntax errors in index

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql");
 const util = require("util");
 const inquirer = require("inquirer");
-
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -11,24 +10,40 @@ const connection = mysql.createConnection({
 });
 
 const init = async function () {
-    const { action } = await inquirer.prompt([{
-        type: "list",
-        name: "action",
-        message: "Which would you like to do?",
-        choices: [
-            "See data",
-            "Update employee",
-            "Create new data",
-            "Delete data",
-            "Quit"
-        ]
-    }]),
+  const { action } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "Which would you like to do?",
+      choices: [
+        "See data",
+        "Update employee",
+        "Create new data",
+        "Delete data",
+      ],
+    },
+  ]);
+};
 
-
-connection.connect();
-
-connection.query = util.promisify(connection.query);
-
-module.exports = connection;
-
+switch (action) {
+  case "See data":
+    await seeData(connection);
+    init();
+    break;
+  case "Create new data":
+    await createData(connection);
+    init();
+    break;
+  case "Update employee":
+    await updateData(connection);
+    init();
+    break;
+  case "Delete data":
+    await deleteData(connection);
+    init();
+    break;
+  default:
+    connection.end();
+    break;
+}
 init();
